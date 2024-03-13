9657054419e7b9acc918cdf45e3187925fa3d328
exleyt <81730686+exleyt@users.noreply.github.com>
2024-03-12 23:00:04 -0700

    Wake sensor before writing to config registers

--- Src/icm20948.c
+++ Src/icm20948.c
@@ -46,6 +46,10 @@ HAL_StatusTypeDef ICM20948_Init(I2C_HandleTypeDef* h_i2c, SDO_Pinouts  SDO_pinou
 
 	HAL_Delay(STARTUP_DELAY);
 
+	status = ICM20948_Wake();
+	if (status != HAL_OK)
+		return status;
+
 	status = ICM20948_WriteRegister(&REG_LP_CONFIG, REG_LP_CONFIG.init_value);
 	if (status != HAL_OK)
 		return status;
@@ -81,13 +85,6 @@ HAL_StatusTypeDef ICM20948_Init(I2C_HandleTypeDef* h_i2c, SDO_Pinouts  SDO_pinou
 	status = ICM20948_WriteRegister(&REG_TEMP_CONFIG, REG_TEMP_CONFIG.init_value);
 	if (status != HAL_OK)
 		return status;
-
-	status = ICM20948_WriteRegister(&REG_PWR_MGMT_1, REG_PWR_MGMT_1.init_value);
-	if (status != HAL_OK)
-		return status;
-
-	if ((REG_PWR_MGMT_1.init_value & 0b01000000) == 0b00000000) // If device is not sleeping
-		HAL_Delay(WAKE_DELAY);
 
 	return HAL_OK;
 }
