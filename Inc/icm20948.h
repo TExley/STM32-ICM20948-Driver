31a7efdfa7b419b01776931f6aa3aa86ea64337a
exleyt <81730686+exleyt@users.noreply.github.com>
2024-06-22 00:24:44 -0700

    Added some interrupt and fsync registers
    ammended reserved mask hotfix to this commit

--- Inc/icm20948.h
+++ Inc/icm20948.h
@@ -125,6 +125,48 @@ typedef enum pwr_mgmt_1_opts
 	DEVICE_RESET = 0b10000000
 } pwr_mgmt_1_opts;
 
+/* BIT 	| NAME 				| DESC
+ * 7 	| INT1_ACTL			| the logic level for INT1 pin active is low (1) or high (0)
+ * 6 	| INT1_OPEN			| INT1 pin is configured as open drain (1) or push-pull (0)
+ * 5 	| INT1_LATCH__EN	| INT1 pin level held until interrupt status is cleared (1) or 50us have passed (0)
+ * 4 	| INT_ANYRD_2CLEAR	| INT_STATUS is cleared if any read is performed (1) or only when INT_STATUS is read (0)
+ * 3 	| ACTL_FSYNC		| logic level for FSYNC pin is active is low (1) or high (0)
+ * 2	| FSYNC_INT_MODE_EN	| 1 enables the FSYNC pin to be used as an interrupt (DS p38)
+ * 1	| BYPASS_EN			| 1 sets ES_CL and ES_DA pins to bypass mode when I2C master interface is disabled
+ * 0	| -					| reserved
+ * Reset Value: 0
+ */
+static const reg_RW REG_INT_PIN_CFG = { "INT_PIN_CFG", 0x0F, UBANK_0, 0b00000001 };
+typedef enum int_pin_cfg_opts
+{
+	BYPASS_EN = 0b00000010,
+	FSYNC_INT_MODE_EN = 0b00000100,
+	ACTL_FSYNC = 0b00001000,
+	INT_ANYRD_2CLEAR = 0b00010000,
+	INT1_LATCH__EN = 0b00100000,
+	INT1_OPEN = 0b01000000,
+	INT1_ACTL = 0b10000000
+} int_pin_cfg_opts;
+
+/* BIT 	| NAME 				| DESC
+ * 7 	| REG_WOF_EN		| 1 enables wake on FSYNC interrupt
+ * 6:4 	| -					| reserved
+ * 3 	| WOM_INT_EN		| 1 enables interrupt for wake on motion to propagate to interrupt pin 1
+ * 2	| PLL_RDY_EN		| 1 enables PLL RDY interrupt to propagate to interrupt pin 1 (DS p39)
+ * 1	| DMP_INT1_EN		| 1 enables DMP interrupt to propagate to interrupt pin 1
+ * 0	| I2C_MST_INT_EN	| 1 enables I2C master interrupt to propagate to interrupt pin
+ * Reset Value: 0
+ */
+static const reg_RW REG_INT_ENABLE = { "INT_ENABLE", 0x10, UBANK_0, 0b01110000 };
+typedef enum int_enable_opts
+{
+	I2C_MST_INT_EN = 0b00000001,
+	DMP_INT1_EN = 0b00000010,
+	PLL_RDY_EN = 0b00000100,
+	WOM_INT_EN = 0b00001000,
+	REG_WOF_EN = 0b10000000
+} int_enable_opts;
+
 static const reg_R REG_ACCEL_XOUT_H = { "ACCEL_XOUT_H", 0x2D, UBANK_0 };
 static const reg_R REG_ACCEL_XOUT_L = { "ACCEL_XOUT_L", 0x2E, UBANK_0 };
 static const reg_R REG_ACCEL_YOUT_H = { "ACCEL_YOUT_H", 0x2F, UBANK_0 };
@@ -161,6 +203,7 @@ typedef enum fifo_en_1_opts
 } fifo_en_1_opts;
 
 /* ICM20948 USER UBANK 0 REGISTERS END */
+
 
 /* ICM20948 USER UBANK 1 REGISTERS START */
 /* BIT 	| NAME 			| DESC
@@ -202,6 +245,7 @@ static const reg_RW REG_ZA_OFFS_H = { "ZA_OFFS_H", 0x1A, UBANK_1, 0 };
  */
 static const reg_RW REG_ZA_OFFS_L = { "ZA_OFFS_L", 0x1B, UBANK_1, 0b00000001 };
 /* ICM20948 USER UBANK 1 REGISTERS END */
+
 
 /* ICM20948 USER UBANK 2 REGISTERS START */
 /* BIT 	| NAME 				| DESC
@@ -364,6 +408,30 @@ typedef enum accel_config_2_opts
 	AX_ST_EN_REG = 0b00010000
 } accel_config_2_opts;
 
+/* BIT 	| NAME 				| DESC
+ * 7 	| DELAY_TIME_EN		| 1 enables delay time measurement between FSYNC event and the first ODR event after FSYNC event
+ * 6	| -					| reserved
+ * 5	| WOF_DEGLITCH_EN	| 1 enables digital deglitching of FSYNC input for wake on FSYNC
+ * 4	| WOF_EDGE_INT		| wake on FSYNC is an edge (1) or level (0) interrupt
+ * 3:0	| EXT_SYNC_SET		| enables the FSYNC pin data to be sampled for the chosen bit sync location (DS p66)
+ * Reset Value: 0
+ */
+static const reg_RW REG_FSYNC_CONFIG = { "FSYNC_CONFIG", 0x52, UBANK_2, 0b01000000 };
+typedef enum fsync_config_opts
+{
+	// EXT_SYNC_DISABLED by default
+	EXT_SYNC_TEMP_OUT_L = 0b00000001,
+	EXT_SYNC_GYRO_XOUT_L = 0b00000010,
+	EXT_SYNC_GYRO_YOUT_L = 0b00000011,
+	EXT_SYNC_GYRO_ZOUT_L = 0b00000100,
+	EXT_SYNC_ACCEL_XOUT_L = 0b00000101,
+	EXT_SYNC_ACCEL_YOUT_L = 0b00000110,
+	EXT_SYNC_ACCEL_ZOUT_L = 0b00000111,
+	WOF_EDGE_INT = 0b00010000,
+	WOF_DEGLITCH_EN = 0b00100000,
+	DELAY_TIME_EN = 0b10000000
+} fsync_config_opts;
+
 /* BIT 	| NAME 			| DESC
  * 7:3 	| -				| unused (not labeled reserved)
  * 2:0	| TEMP_DLPFCFG	| low pass filter config (DS p67)
@@ -382,6 +450,7 @@ typedef enum temp_config_opts
 	TEMP_DLPFCFG_7 = 0b00000111
 } temp_config_opts;
 /* ICM20948 USER UBANK 2 REGISTERS END */
+
 
 /* ICM20948 USER UBANK 3 REGISTERS START */
 /* BIT 	| NAME 					| DESC
@@ -420,6 +489,27 @@ typedef enum i2c_mst_ctrl_opts
 	I2C_MST_P_NSR = 0b00010000,
 	MULT_MST_EN = 0b10000000
 } i2c_mst_ctrl_opts;
+
+/* BIT 	| NAME 				| DESC
+ * 7 	| DELAY_ES_SHADOW	| Delays shadowing of external sensor data until all data is received
+ * 6:5	| -					| reserved
+ * 4	| I2C_SLV4_DELAY_EN	| 1 sets slave 4 to only be accessed 1/(1+I2C_SLC4_DLY) samples
+ * 3	| I2C_SLV3_DELAY_EN	| 1 sets slave 3 to only be accessed 1/(1+I2C_SLC4_DLY) samples
+ * 2	| I2C_SLV2_DELAY_EN	| 1 sets slave 2 to only be accessed 1/(1+I2C_SLC4_DLY) samples
+ * 1	| I2C_SLV1_DELAY_EN	| 1 sets slave 1 to only be accessed 1/(1+I2C_SLC4_DLY) samples
+ * 0	| I2C_SLV0_DELAY_EN	| 1 sets slave 0 to only be accessed 1/(1+I2C_SLC4_DLY) samples
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_MST_DELAY_CTRL = { "I2C_MST_DELAY_CTRL", 0x02, UBANK_3, 0b01100000 };
+typedef enum i2c_mst_delay_ctrl_opts
+{
+	I2C_SLV0_DELAY_EN = 0b00000001,
+	I2C_SLV1_DELAY_EN = 0b00000010,
+	I2C_SLV2_DELAY_EN = 0b00000100,
+	I2C_SLV3_DELAY_EN = 0b00001000,
+	I2C_SLV4_DELAY_EN = 0b00010000,
+	DELAY_ES_SHADOW = 0b10000000
+} i2c_mst_delay_ctrl_opts;
 
 /* BIT 	| NAME 			| DESC
  * 7 	| I2C_SLV0_RNW	| 1/0 – Transfer is a read/write
@@ -477,6 +567,103 @@ typedef enum i2c_slv_ctrl_opts
  */
 static const reg_RW REG_I2C_SLV0_DO = { "I2C_SLV0_DO", 0x06, UBANK_3, 0 };
 
+/* BIT 	| NAME 			| DESC
+ * 7 	| I2C_SLV1_RNW	| 1/0 – Transfer is a read/write
+ * 6:0	| I2C_ID_1		| physical address of I2C slave 1
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV1_ADDR = { "I2C_SLV1_ADDR", 0x07, UBANK_3, 0 };
+
+/* BIT 	| NAME 			| DESC
+ * 7:0 	| I2C_SLV1_REG	| register address from where to begin data transfer
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV1_REG = { "I2C_SLV1_REG", 0x08, UBANK_3, 0 };
+
+/* BIT 	| NAME 				| DESC
+ * 7 	| I2C_SLV1_EN		| 1 reads data at the sample rate and stores it at the first available EXT_SENS_DATA register
+ * 6 	| I2C_SLV1_BYTE_SW	| 1 enables swapping bytes when reading both the low and high byte of a word (DS p70)
+ * 5 	| I2C_SLV1_REG_DIS	| 1 the transaction does not write a register value, it will only read data, or write data
+ * 4 	| I2C_SLV1_GRP		| sets wether groups of bytes swapped end in odd (0) or even (1) registers
+ * 3:0 	| I2C_SLV1_LENG		| number of bytes to be read from I2C slave 1
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV1_CTRL = { "I2C_SLV1_CTRL", 0x09, UBANK_3, 0 };
+
+/* BIT 	| NAME 			| DESC
+ * 7:0 	| I2C_SLV1_DO	| data out when slave 1 is set to write
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV1_DO = { "I2C_SLV1_DO", 0x0A, UBANK_3, 0 };
+
+/* BIT 	| NAME 			| DESC
+ * 7 	| I2C_SLV4_RNW	| 1/0 – Transfer is a read/write
+ * 6:0	| I2C_ID_4		| physical address of I2C slave 4
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV4_ADDR = { "I2C_SLV4_ADDR", 0x13, UBANK_3, 0 };
+
+/* BIT 	| NAME 			| DESC
+ * 7:0 	| I2C_SLV4_REG	| register address from where to begin data transfer
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV4_REG = { "I2C_SLV4_REG", 0x14, UBANK_3, 0 };
+
+/* BIT 	| NAME 				| DESC
+ * 7 	| I2C_SLV4_EN		| 1 data transfer at the sample rate and stores read data in I2C_SLV4_DI, bit clears afterwards
+ * 6 	| I2C_SLV4_INT_EN	| 1 enables the completion of the I2C slave 4 data transfer to cause an interrupt
+ * 5 	| I2C_SLV4_REG_DIS	| 1 the transaction does not write a register value, it will only read data, or write data
+ * 4:0 	| I2C_SLV4_DLY		| when enabled via the I2C_MST_DELAY_CTRL, those slaves will only be enabled every 1/(1+I2C_SLV4_DLY)
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV4_CTRL = { "I2C_SLV4_CTRL", 0x15, UBANK_3, 0 };
+typedef enum i2c_slv4_ctrl_opts
+{
+	I2C_SLV4_DLY_1 = 0b00000001,
+	I2C_SLV4_DLY_2 = 0b00000010,
+	I2C_SLV4_DLY_3 = 0b00000011,
+	I2C_SLV4_DLY_4 = 0b00000100,
+	I2C_SLV4_DLY_5 = 0b00000101,
+	I2C_SLV4_DLY_6 = 0b00000110,
+	I2C_SLV4_DLY_7 = 0b00000111,
+	I2C_SLV4_DLY_8 = 0b00001000,
+	I2C_SLV4_DLY_9 = 0b00001001,
+	I2C_SLV4_DLY_10 = 0b00001010,
+	I2C_SLV4_DLY_11 = 0b00001011,
+	I2C_SLV4_DLY_12 = 0b00001100,
+	I2C_SLV4_DLY_13 = 0b00001101,
+	I2C_SLV4_DLY_14 = 0b00001110,
+	I2C_SLV4_DLY_15 = 0b00001111,
+	I2C_SLV4_DLY_16 = 0b00010000,
+	I2C_SLV4_DLY_17 = 0b00010001,
+	I2C_SLV4_DLY_18 = 0b00010010,
+	I2C_SLV4_DLY_19 = 0b00010011,
+	I2C_SLV4_DLY_20 = 0b00010100,
+	I2C_SLV4_DLY_21 = 0b00010101,
+	I2C_SLV4_DLY_22 = 0b00010110,
+	I2C_SLV4_DLY_23 = 0b00010111,
+	I2C_SLV4_DLY_24 = 0b00011000,
+	I2C_SLV4_DLY_25 = 0b00011001,
+	I2C_SLV4_DLY_26 = 0b00011010,
+	I2C_SLV4_DLY_27 = 0b00011011,
+	I2C_SLV4_DLY_28 = 0b00011100,
+	I2C_SLV4_DLY_29 = 0b00011101,
+	I2C_SLV4_DLY_30 = 0b00011110,
+	I2C_SLV4_DLY_31 = 0b00011111,
+	I2C_SLV4_INT_EN = 0b01000000,
+} i2c_slv4_ctrl_opts;
+
+/* BIT 	| NAME 			| DESC
+ * 7:0 	| I2C_SLV4_DO	| data out when slave 4 is set to write
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV4_DO = { "I2C_SLV4_DO", 0x16, UBANK_3, 0 };
+
+/* BIT 	| NAME 			| DESC
+ * 7:0 	| I2C_SLV4_DI	| data in when slave 4 is set to read
+ * Reset Value: 0
+ */
+static const reg_RW REG_I2C_SLV4_DI = { "I2C_SLV4_DI", 0x17, UBANK_3, 0 };
 /* ICM20948 USER UBANK 3 REGISTERS END */
 
 /* SENSOR SUPPORT STRUCTS START */
